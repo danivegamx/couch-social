ff6aa1bfdd39a6940e7c2002b5da6110236e289c
Daniel Vega <ingdanivega@gmail.com>
2025-01-28 20:39:25 -0600

    Fixed deployment

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,15 +2,6 @@
 
 import Image from "next/image";
 import PostCard from "../../components/posts/PostCard";
-// import {
-//   FiMessageCircle,
-//   FiUsers,
-//   FiUser,
-//   FiHome,
-//   FiBell,
-//   FiLogOut,
-//   FiSettings
-// } from "react-icons/fi";
 import { usePosts } from "../../context/postContext";
 import { useUser } from "../../context/userContext";
 
@@ -18,7 +9,6 @@ import "./styles.sass";
 
 export default function Home() {
   const { currentUser } = useUser();
-  console.log(currentUser)
   const { posts } = usePosts();
   const userPosts = posts.filter(post => post.userId === currentUser?.id);
 
@@ -38,7 +28,7 @@ export default function Home() {
         <div className="profile-details md:mt-4 text-center md:text-left">
           <h3 className="font-bold text-2xl">{currentUser?.name}</h3>
           <p className="text-gray-500">{currentUser?.location}</p>
-          <p className="profile-bio"><span>" </span>{currentUser?.bio}</p>
+          <p className="profile-bio"><span></span>{currentUser?.bio}</p>
         </div>
         <div className="profile-stats grid grid-cols-3 gap-2 pt-5 md:ml-[auto]">
           <div className="profile-stats-item rounded-2xl"><span>{userPosts.length}</span> post{userPosts.length > 1 ? 's' : ''}</div>
